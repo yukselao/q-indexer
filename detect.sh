7ebd472417359a7ab379aee1b0a479e428b353a0
U-IBM\aliokan <aliokan@IBM-JumpAliOkan.security.tr.ibm.com>
2022-01-03 23:36:55 +0300

    commit

--- detect.sh
+++ detect.sh
@@ -5,6 +5,7 @@ enddt="$2"
 testip="$3"
 sessionname="$4"
 key="$5"
+host="$6"
 startts="$(date "+%s" -d "$startdt")"
 endts="$(date "+%s" -d "$enddt")"
 
@@ -15,7 +16,9 @@ plog INFO "endts=$endts ($enddt)"
 
 function print_usage() {
 	echo 'Usage:'$0 \ 
-'"2021-12-31 10:00:00" "2021-12-31 10:59:00" 0.5.0.1'
+'"2021-12-31 10:00:00" "2021-12-31 10:59:00" 0.5.0.1' \
+'"2021-12-31 10:00:00" "2021-12-31 10:59:00" 0.5.0.1 "10.10.2.10:32011" (for AIO)'
+'"2021-12-31 10:00:00" "2021-12-31 10:59:00" 0.5.0.1 "10.10.2.10:32006" (for EC or Datanode)'
 }
 
 fark="$(expr $(expr $endts - $startts) / 86400)"
@@ -27,12 +30,16 @@ for d in $(seq 0 $fark); do
 	mydaystart="$(date "+%F" -d @$(expr $startts + $secs + 86400))"
 	mydayend="$(date "+%F" -d @$(expr $startts + $secs + 86400))"
 	#plog INFO "Processing $mydaystart ..."
-	cmd="./qactl -checkdatafilecount $key '$mydaystart 00:00:00' '$mydayend 23:59:59' $testip"
+	if [[ ! -z "$host" ]]; then
+		cmd="./qactl -checkdatafilecount $key '$mydaystart 00:00:00' '$mydayend 23:59:59' $testip $host"
+	else
+		cmd="./qactl -checkdatafilecount $key '$mydaystart 00:00:00' '$mydayend 23:59:59' $testip"
+	fi
 	#plog INFO "$cmd"
 	stats="$(eval $cmd | grep FileCount | paste - - | sed -r 's#.+dataFileCount is ([0-9]+).+indexFileCount is ([0-9]+).+#\1;\2#')"
 	datastat="$(echo $stats | awk -F';' '{print $1}')"
 	indexstat="$(echo $stats | awk -F';' '{print $2}')"
-	wlog "$(date "+%F %T");$mydaystart;dataFilecount:$datastat;indexFileCount:$indexstat;$cmd"
+	wlog "$mydaystart;dataFilecount:$datastat;indexFileCount:$indexstat;$cmd"
 	if [[ "$datastat" -gt 3 ]]; then
 		plog "WARNING" "index corruption detected on $mydaystart"
 	fi
