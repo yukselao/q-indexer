ba9714bb371543ed9bf0bbb6f57ede1ff69598c9
root <root@qradar.offense-lab.ibm.com>
2022-01-07 15:14:10 +0300

    commit

--- get-all-searches-v7.4.3.sh
+++ get-all-searches-v7.4.3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-
 #aql='select "AQL Statement", "Ariel Cursor ID" from events where (qid=28250254 AND userName='"'"commandapiuser"'"') LAST 24 HOURS'
 aql='select "payload" from events where (qid=28250254 AND userName='"'"apiuser"'"') LAST 24 HOURS'
-echo "INFO Get Ariel Cursor IDs with aql -> $aql"
-/opt/qradar/bin/ariel_query -f /opt/q-indexer/token -q "$aql" 2>/dev/null |grep -v Cursor
+for line in $(/opt/qradar/bin/ariel_query -f /opt/q-indexer/token -q "$aql" 2>/dev/null |grep -v Cursor | grep -v payload); do
+	echo $line | base64 -d | sed -r 's#.+Params:Id:(.+), DB:.+#\1#'
+done
