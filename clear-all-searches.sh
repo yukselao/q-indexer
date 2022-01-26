db05be2d547b2ac9fe55bc8f62262899c70ce068
root <root@qradar.offense-lab.ibm.com>
2022-01-26 22:58:37 +0300

    updated

--- clear-all-searches.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for searchid in $(./get-all-searches-v7.4.3.sh); do
-	./qactl -deletesearch $searchid 
-done
--- get-all-searches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-#aql='select "AQL Statement", "Ariel Cursor ID" from events where (qid=28250254 AND userName='"'"commandapiuser"'"') LAST 24 HOURS'
-aql='select "payload" from events where (qid=28250254 AND userName='"'"apiuser"'"') LAST 24 HOURS'
-echo "INFO Get Ariel Cursor IDs with aql -> $aql"
-/opt/qradar/bin/ariel_query -f /opt/q-indexer/token -q "$aql" 2>/dev/null |grep -v Cursor
